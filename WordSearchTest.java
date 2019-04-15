1d0151f33cf3d854cd67b87734ee5fe01cd2766e
zdrobenko <36285589+zdrobenko@users.noreply.github.com>
2019-04-15 14:50:36 -0500

    Write tests for RightToLeft
    Initially test will not pass due to no function added yet

--- WordSearchTest.java
+++ WordSearchTest.java
@@ -5,10 +5,19 @@ import main.WordSearch;
 
 public class WordSearchTest {
 
+	//test for left to right reading
 	@Test
 	public void LeftToRigth() {
 		WordSearch leftToRight = new WordSearch();
-		String word = leftToRight.leftToRight("BONES");
-		assertEquals("BONES", word);
+		String word = leftToRight.leftToRight("SCOTTY");
+		assertEquals("SCOTTY", word);
+	}
+	
+	//test for right to left reading
+	@Test
+	public void RightToLeft () {
+		WordSearch rightToLeft = new WordSearch();
+		String word = rightToLeft.rightToLeft("");
+		assertEquals("", word);
 	}
 }
