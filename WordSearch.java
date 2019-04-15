96064aa916c3fed999eb76800509664fab4a0738
zdrobenko <36285589+zdrobenko@users.noreply.github.com>
2019-04-15 15:02:28 -0500

    Write function for reading Right to Left
    this function reads backwards. All tests pass

--- WordSearch.java
+++ WordSearch.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 public class WordSearch {
 	
 	//create a 2D array for the grid
-	static char grid[][] = {{}};
+	static char grid[][] = {{'U','M','K','H','U','L', 'K','I','N','V','J','O','C','W','E'},
+		{'L','L','S','H','K','Z','Z','W','Z','C','G','J','U','Y','G'},
+		{'H','S','U','P','J','P','R','J','D','H','S','B','X','T','G'},
+		{'B','R','J','S','O','E','Q','E','T','I','K','K','G','L','E'},
+		{'A','Y','O','A','G','C','I','R','D','Q','H','R','T','C','D'},
+		{'S','C','O','T','T','Y','K','Z','R','E','P','P','X','P','F'},
+		{'B','L','Q','S','L','N','E','E','E','V','U','L','F','M','Z'},
+		{'O','K','R','I','K','A','M','M','R','M','F','B','A','P','P'},
+		{'N','U','I','I','Y','H','Q','M','E','M','Q','R','Y','F','S'},
+		{'E','Y','Z','Y','G','K','Q','J','P','C','Q','W','Y','A','K'},
+		{'S','J','F','Z','M','Q','I','B','D','B','E','M','K','W','D'},
+		{'T','G','L','B','H','C','B','E','C','H','T','O','Y','I','K'},
+		{'O','J','Y','E','U','L','N','C','C','L','Y','B','Z','U','H'},
+		{'W','Z','M','I','S','U','K','U','R','B','I','D','U','X','S'},
+		{'K','Y','L','B','Q','Q','P','M','D','F','C','K','E','A','B'}};
 	
 	public static void main(String[] args) {
 		//I see what you guys did there bringing in Star Trek
@@ -30,7 +44,8 @@ public class WordSearch {
 		//TODO: need to write all of the functions 
 		//to make this work
 		for(String search : wordSearch) {
-			
+			leftToRight(search);
+			rightToLeft(search);
 		}
 	}
 	
@@ -42,5 +57,65 @@ public class WordSearch {
 			}
 			System.out.println("");
 		}
+	}
+	
+	//first right the function to find words left to right in rows
+	//@Params word = the word to search
+	public static String leftToRight(String word) {
+		//create a counter for the the letters
+		int counter = 0;
+		String wordSearch = "";
+		
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (word.charAt(counter) == grid[i][j]) {
+					counter++;
+				}
+				else {
+					counter = 0;
+				}
+				if (counter == word.length()) {
+					System.out.println(word + ":" + "\t");
+					for (int col = j - counter + 1; col <= j; col++) {
+						System.out.println(" " + "(" + col + "," + i + ")");
+						counter = 0;
+					}
+					System.out.println("");
+					wordSearch = word;
+				}
+			}	
+		}
+		return wordSearch;
+	}
+	
+	//write the function to read from Right to Left
+	//this will be the opposite of Left to Right
+	//@Params word = the word to search
+	public static String rightToLeft(String word) {
+		//create a counter for the letters
+		int counter = 0;
+		String wordSearch = "";
+		for (int i = 0; i < grid.length; i++) {
+			//going backwards now
+			for (int j = grid[i].length - 1; j >= 0; j--) {
+				if (word.charAt(counter) == grid[i][j]) {
+					counter++;
+				}
+				else {
+					counter = 0;
+				}
+				if (counter == word.length()) {
+					System.out.println(word + ":");
+					for (int col = j + counter - 1; col >= j; col--) {
+						System.out.println(" " + "(" + col + "," + i + ")");
+						counter = 0;
+					}
+					System.out.println("");
+					wordSearch = word;
+				}
+			}
+		}
+		
+		return wordSearch;
 	}
 }
